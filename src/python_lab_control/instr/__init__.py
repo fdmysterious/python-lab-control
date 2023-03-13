42afba975646c5b00c2ff8224f4a012df8bb6067
pantostier <quentin.pantostier@elsys-design.com>
2023-03-13 11:59:13 +0100

    remove import from init.py

--- src/python_lab_control/instr/__init__.py
+++ src/python_lab_control/instr/__init__.py
@@ -5,8 +5,3 @@ Python lab control
 :Authors: - Quentin Pantostier
 :Date: Mars 2023
 """
-
-from .tds2024b import TDS2024B_Interface
-from .afg3000  import AFG3000_Interface
-from .dpo4034  import DPO4034_Interface
-from .ql335p   import QL335P_Interface
