eb5bc4d2793d6e910af939f7baafb916043a0c5f
pantostier <quentin.pantostier@elsys-design.com>
2023-03-13 10:52:04 +0100

    Add python package files

---
+++ instr/__init__.py
@@ -0,0 +1,12 @@
+"""
+===================================
+Python lab control
+===================================
+:Authors: - Quentin Pantostier
+:Date: Mars 2023
+"""
+
+from instr import tds2024b
+from instr import afg3000
+from instr import dpo4034
+from instr import ql335p
