1ece714643d1a1487a2647d3374ff7c05a8aa599
pantostier <quentin.pantostier@elsys-design.com>
2023-03-13 11:41:15 +0100

    Test move import

--- src/python_lab_control/__init__.py
+++ src/python_lab_control/__init__.py
@@ -5,3 +5,8 @@ Python lab control
 :Authors: - Quentin Pantostier
 :Date: Mars 2023
 """
+
+from instr.tds2024b import TDS2024B_Interface
+from instr.afg3000  import AFG3000_Interface
+from instr.dpo4034  import DPO4034_Interface
+from instr.ql335p   import QL335P_Interface
